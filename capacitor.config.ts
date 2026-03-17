97243039bcc8919ed404f40c82f88070310e234d
sla15 <sladibba15@gmail.com>
2026-03-17 05:52:11 +0000

    for ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -8,6 +8,10 @@ const config: CapacitorConfig = {
     PushNotifications: {
       presentationOptions: ["badge", "sound", "alert"],
     },
+    Keyboard: {
+      resize: "native",
+      resizeOnFullScreen: true,
+    },
   },
 };
 
